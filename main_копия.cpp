8da2915a6b7f1d2c012df569745d971659f4ecb3
Anastasia Matytsina <matytsina.nastenka@mail.ru>
2023-05-31 22:30:18 +0700

    Создала репозиторий
    Начала разработку

--- main_копия.cpp
+++ main_копия.cpp
@@ -1,10 +1,8 @@
 #include <QCoreApplication>
-#include "win.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    Win win(0);
-    win.show();
+
     return a.exec();
 }
--- win.h
+++ win.h
@@ -1,11 +1,30 @@
-#ifndef WIN_H
-#define WIN_H
-
-
-class win
+#ifndef win_h
+#define win_h
+#include <QtGui>
+class Win:public QWidget
+{
+Q_OBJECT // макрос Qt, обеспечивающий корректное создание сигналов и слотов
+protected:
+QTextCodec *codec;
+QFrame *frame; // рамка
+QLabel *inputLabel; // метка ввода
+QLineEdit *inputEdit; // строчный редактор ввода
+QLabel *outputLabel; // метка вывода
+QLineEdit *outputEdit; // строчный редактор вывода
+QPushButton *nextButton; // кнопка Следующее
+QPushButton *exitButton; // кнопка Выход
+public:
+Win(QWidget *parent = 0); // конструктор
+public slots:
+void begin(); // метод начальной настройки интерфейса
+void calc(); // метод реализации вычислений
+};
+class StrValidator:public QValidator // класс компонента проверки ввода
 {
 public:
-    win();
+StrValidator(QObject *parent):QValidator(parent){}
+virtual State validate(QString &str,int &pos)const
+{
+}
 };
-
-#endif // WIN_H
+#endif
